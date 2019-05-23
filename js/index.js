cfd025fa86a5f047b0af058cfc4dd98358ade168
LadyKerr <kadiekerr@gmail.com>
2019-05-22 21:06:06 -0400

    started implementing js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+//change color of nav links
+const navLink = document.querySelectorAll('a');
+navLink.forEach(link => {
+  link.addEventListener('mouseover', (event) => {
+    link.style.color = 'red';
+  });
+  link.addEventListener('mouseout', (event) => {
+    link.style.color = 'black';
+  });
+  link.preventDefault;
+});
+
+//style button
+  const btn = document.querySelector('.btn');
+  btn.addEventListener('mouseover', (event) => {
+    btn.style.backgroundColor = 'white';
+    btn.style.color = 'black';
+  });
+  btn.addEventListener('mouseout', (event) => {
+    btn.style.backgroundColor = '#fcbe03';
+    btn.style.color = 'white';
+  });
